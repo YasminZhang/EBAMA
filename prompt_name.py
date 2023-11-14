c492ed49864fced771367ac686e6d3ecf6a32dc8
Yasi <yasminzhang@ucla.edu>
2023-11-14 13:16:28 -0800

    add a file

---
+++ prompt_name.py
@@ -0,0 +1,9 @@
+PROMPTS = ['a gray crown and a purple apple']
+
+SEEDS = [32,42,43,53]
+# 'a checkered balloon and a sliced strawberry and a purple banana','a modern crown and a skewered apple and a gray dog', 'a teal tomato and a furry mouse and a brown wooden camera',\
+#             'a purple baby horse and a black banana and a sliced tomato', 'a purple modern camera and a spotted baby dog and a sliced tomato',\
+#                  'a red metal crown and a white bear and a wooden chair', 'a green banana and a spotted chair and a red baby furry bear', 'a baby monkey and a sliced tomato and a blue baby gorilla' 
+# 'an orange suitcase and a sliced strawberry and a baby mouse', \
+#            'a spotted turtle and a purple apple and a sliced blue tomato',  'a skewered strawberry and a checkered clock and a spotted sleepy penguin', \
+#                   'a sliced apple and a purple camera and a teal lion', 
